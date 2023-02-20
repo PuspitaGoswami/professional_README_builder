db94ccf32e66350ad52a7a298d85c6c0c64b435f
Your Name <you@example.com>
2023-02-20 00:30:11 +0000

    Show badge for license

--- index.js
+++ index.js
@@ -61,15 +61,15 @@ const promptUser = () =>
 //functions to manipulate badges and send user to license page
 
 //show badge function to indicate the license a user opted out for
-// function showBadge(license) {
-//   const diffBadge = {
-//     gnuplv3:
-//       "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.html)",
-//     isc: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
-//     mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensources.org/MIT)",
-//   };
-//   return diffBadge[license];
-// }
+function showBadge(license) {
+  const diffBadge = {
+    gnuplv3:
+      "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.html)",
+    isc: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+    mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensources.org/MIT)",
+  };
+  return diffBadge[license];
+}
 
 // //second function:
 
